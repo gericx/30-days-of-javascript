8d6b82a6f3fb226650370123bfc34508f3c9c165
Aleksandra Geric <146990325+gericx@users.noreply.github.com>
2026-09-12 22:11:53 +0200

    Add Return Length of Arguments Passed function

---
+++ return-length-of-arguments-passed.ts
@@ -0,0 +1,8 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+function argumentsLength(...args: JSONValue[]): number {
+    let count: number = 0;
+    
+    for (let i=0; i<args.length; i++) count++
+    return count
+};
